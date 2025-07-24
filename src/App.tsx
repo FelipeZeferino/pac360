4feab03bcab45d16d377336d83981db129157d61
FelipeZeferino <fzeferino16@gmail.com>
2025-07-24 16:52:39 -0300

    added not-found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UserDetails from "./pages/UserInfo";
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user/:id" element={<UserDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
